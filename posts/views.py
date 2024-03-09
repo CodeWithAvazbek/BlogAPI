312914a0499c64a0516566a6f0aaf750e6a10d69
programmingavazbek@gmail.com <hazratovavazbek994104879@gmail.com>
2024-03-09 12:03:54 +0500

    nimadurlar

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
-from .serializers import PostSerializers
+from .serializers import PostSerializers, UserSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Post
 from rest_framework import permissions
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
 
 
 class PostList(ListCreateAPIView):
@@ -14,3 +15,13 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = IsAuthorOrReadOnly,
     queryset = Post.objects.all()
     serializer_class = PostSerializers
+
+
+class UserList(ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
