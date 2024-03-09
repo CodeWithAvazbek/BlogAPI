b7459615f4a7ce776091cb994fd6d8e3aa36df3c
programmingavazbek@gmail.com <hazratovavazbek994104879@gmail.com>
2024-03-09 15:03:15 +0500

    ready

--- posts/views.py
+++ posts/views.py
@@ -4,24 +4,35 @@ from .models import Post
 from rest_framework import permissions
 from .permissions import IsAuthorOrReadOnly
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 
 
-class PostList(ListCreateAPIView):
+# class PostList(ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializers
+#
+#
+# class PostDetail(RetrieveUpdateDestroyAPIView):
+#     permission_classes = IsAuthorOrReadOnly,
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializers
+#
+#
+# class UserList(ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class UserDetail(RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializers
 
 
-class PostDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = IsAuthorOrReadOnly,
-    queryset = Post.objects.all()
-    serializer_class = PostSerializers
-
-
-class UserList(ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog Api",
+        description="Django rest frameworkni urganish uchun",
+        default_version='v1',
+        terms_of_service="",
+        contact=openapi.Contact(email="programmingavazbek@gmail.com"),
+        license=openapi.License(name="Blog project litsensiyasi"),
+    ),
+    public=True,
+    permission_classes = (permissions.AllowAny,)
+)
 
 urlpatterns = [
 
@@ -8,6 +24,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api/allauth/', include('allauth.urls'))
+    path('api/allauth/', include('allauth.urls')),
 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
